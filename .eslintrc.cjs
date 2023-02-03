52d578ea48dd777e2150c40cd36e2103dd2bb131
WojciechRzoska <wojciechrzoska@gmail.com>
2023-02-03 12:34:50 +0100

    add configurations and delete fiels

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,13 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
 };
